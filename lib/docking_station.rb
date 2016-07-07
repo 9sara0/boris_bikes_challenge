2276507b0a50a74de592906891df17f92f7d45a8
Luke Cartledge <luke@lukecartledge.com>
2016-07-07 16:33:26 +0100

    Passes task 15 - Single Responsibility Principle

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,18 +1,28 @@
 class DockingStation
 
   def initialize
-    @bikes =[]
+    @bikes = []
   end
 
   attr_reader :bikes
 
   def release_bike
-    raise 'No bikes available' unless !@bikes.empty?
+    raise 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    raise 'There is no room to dock a bike' if @bikes.length >= 20
+    raise 'There is no room to dock a bike' if full?
     @bikes << bike
+  end
+
+  private
+
+  def empty?
+    @bikes.empty?
+  end
+
+  def full?
+    @bikes.length >= 20
   end
 end
