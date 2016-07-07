465abb241cae5521440efa5c64ad3b5299c955f2
Luke Cartledge <luke@lukecartledge.com>
2016-07-07 14:54:30 +0100

    passes test - raises error when no bikes docked

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,7 +4,7 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'checks bike is working?' do
-    bike = subject.release_bike
+    bike = Bike.new
     expect(bike).to be_working
   end
 
@@ -16,5 +16,15 @@ describe DockingStation do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
+  end
+  describe ':release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+    it 'raises an error' do
+      expect { subject.release_bike }.to raise_error('No bikes available')
+    end
   end
 end
