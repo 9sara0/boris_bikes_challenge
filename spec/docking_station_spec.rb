bcd742033c749d12a188956fe959159407dc7f87
Sara <sarah.alalaya@gmail.com>
2016-09-28 17:30:59 +0300

    A DockingStation has a capacity of 20

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,31 +3,31 @@ require 'docking_station'
 
 describe DockingStation do
 
-it "docks a bike" do
-  bike = Bike.new
-  subject.dock(bike)
-  expect(subject.bike).to eq bike
-end
+  describe "#release_bike" do
+    it "Releases a working bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
 
-describe "#release_bike" do
-  it "Releases a working bike" do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.release_bike).to eq bike
+    it "Raises an error when no bikes available" do
+      expect{subject.release_bike}.to raise_error 'No bikes available'
+    end
   end
 
-  it "Raises an error when no bikes available" do
-    expect{subject.release_bike}.to raise_error 'No bikes available'
-  end
-end
+  describe "#dock" do
 
+    it "docks a bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bikes).to include bike
+    end
 
-describe "#dock" do
-  # assuming a max capacity of 1 bike
-  it 'raises an error when full' do
-    subject.dock(Bike.new)
-    expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    # assuming a max capacity of 20 bike
+    it 'raises an error when full' do
+      20.times { subject.dock(Bike.new) }
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
   end
-end
 
 end
