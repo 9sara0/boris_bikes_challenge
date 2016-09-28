48b3c2a861dce5a208390786556084643b29f56b
Sara <sarah.alalaya@gmail.com>
2016-09-28 20:49:14 +0300

    a bike can be reported as broken

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,4 +1,17 @@
 require 'bike'
 describe Bike do
+  subject(:bike) { described_class.new }
+
+  describe "upon initialization" do
+    it 'is always working' do
+      expect(bike).to be_working
+    end
+  end
+
   it { is_expected.to respond_to :working? }
+
+  it 'can be reported as a broken bike' do
+    bike.report_broken
+    expect(bike).not_to be_working
+  end
 end
