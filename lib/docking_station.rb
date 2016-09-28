b3eb23a91662dda23a0f384991f6a0799f5ad0f1
Sara <sarah.alalaya@gmail.com>
2016-09-28 20:31:54 +0300

    Capacity can be specified upon instantiation

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,10 +4,11 @@ class DockingStation
 
   DEFAULT_CAPACITY = 20
 
-  attr_reader :bikes
+  attr_reader :bikes, :capacity
 
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
     @bikes = []
+    @capacity = capacity
   end
 
   def release_bike
@@ -27,7 +28,7 @@ class DockingStation
   end
 
   def full?
-    @bikes.count >= DEFAULT_CAPACITY
+    @bikes.count >= @capacity
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,30 +3,44 @@ require 'docking_station'
 
 describe DockingStation do
 
+  subject(:docking_station) { described_class.new }
+  let(:bike) { Bike.new }
+
+  describe "initialization" do
+    it 'default capacity' do
+      expect(docking_station.capacity).to eq described_class::DEFAULT_CAPACITY
+    end
+
+    it 'has a variable capacity' do
+      docking_station = described_class.new(50)
+      50.times { docking_station.dock Bike.new }
+      expect{ docking_station.dock Bike.new }.to raise_error 'Docking station full'
+    end
+  end
+
   describe "#release_bike" do
     it "Releases a working bike" do
       bike = Bike.new
-      subject.dock(bike)
-      expect(subject.release_bike).to eq bike
+      docking_station.dock(bike)
+      expect(docking_station.release_bike).to eq bike
     end
 
     it "Raises an error when no bikes available" do
-      expect{subject.release_bike}.to raise_error 'No bikes available'
+      expect{docking_station.release_bike}.to raise_error 'No bikes available'
     end
   end
 
   describe "#dock" do
-
     it "docks a bike" do
       bike = Bike.new
-      subject.dock(bike)
-      expect(subject.bikes).to include bike
+      docking_station.dock(bike)
+      expect(docking_station.bikes).to include bike
     end
 
     # assuming a max capacity of 20 bike
     it 'raises an error when full' do
-      DockingStation::DEFAULT_CAPACITY.times { subject.dock(Bike.new) }
-      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+      docking_station.capacity.times { docking_station.dock(Bike.new) }
+      expect { docking_station.dock Bike.new }.to raise_error 'Docking station full'
     end
   end
 
