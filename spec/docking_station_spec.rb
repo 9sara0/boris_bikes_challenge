da13bbfa127d843a0254c4d53f9708767c30fb45
Sara <sarah.alalaya@gmail.com>
2016-09-28 16:44:28 +0300

    Raises an error when releasing bike from an empty DockingStation

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,17 +3,22 @@ require 'docking_station'
 
 describe DockingStation do
 
-it { is_expected.to respond_to :release_bike }
-
-it "releases a working bike" do
-  bike = subject.release_bike
-  expect(bike).to be_working
-end
-
 it "docks a bike" do
   bike = Bike.new
   subject.dock(bike)
   expect(subject.bike).to eq bike
+end
+
+describe "#release_bike" do
+  it "Releases a working bike" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.release_bike).to eq bike
+  end
+
+  it "Raises an error when no bikes available" do
+    expect{subject.release_bike}.to raise_error 'No bikes available'
+  end
 end
 
 end
